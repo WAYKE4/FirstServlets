2d3b2fc66c990378e97cd5f35ade0b6c0512c3e7
WAYKE4 <gitta.tacrhi@mail.ru>
2024-01-11 17:40:16 +0300

    Annotation

--- src/main/java/TimeServletBeijing.java
+++ src/main/java/TimeServletBeijing.java
@@ -1,4 +1,5 @@
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -8,7 +9,25 @@ import java.io.PrintWriter;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
+@WebServlet(value = "/beijing")
+
 public class TimeServletBeijing extends HttpServlet {
+    @Override
+    public void init() {
+        System.out.println("THis is INIT method!");
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.service(req, resp);
+        System.out.println("This is SERVICe method! ");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("This is DESTROY METHOD!");
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/html");
--- src/main/java/TimeServletMinsk.java
+++ src/main/java/TimeServletMinsk.java
@@ -1,3 +1,5 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -7,7 +9,24 @@ import java.io.PrintWriter;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
+@WebServlet(value = "/minsk")
 public class TimeServletMinsk extends HttpServlet {
+    @Override
+    public void init() {
+        System.out.println("THis is INIT method!");
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.service(req, resp);
+        System.out.println("This is SERVICe method! ");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("This is DESTROY METHOD!");
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/html");
